7474f95bc35d5122ca92f8640a83a64262b54177
Janetsomehow <janetsomehow@gmail.com>
2020-02-19 03:41:02 +0100

    modal event

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+ <div id="app">
+  <button id="show-modal" @click="showModal = true"> 
+    Open for a surprise
+  </button>
+  <modal v-if="showModal" @close="showModal = false">
+    <h3 slot="header">Hey There!</h3>
+  </modal>
+ </div>
+</template>
+
+<script>
+import modal from './components/modal.vue';
+export default {
+  name: "app",
+  components: {
+    modal,
+  },
+  data() {
+    return {
+      showModal: false,
+    };
+  }
+}
+</script>
+
+<style>
+ h3 {
+   color: #4AAE9B;
+ }
+ #show-modal {
+   background: #4AAE9B;
+    color: white;
+    border-radius: 3px;
+ }
+</style>
---
+++ src/components/modal.vue
@@ -0,0 +1,100 @@
+<template>
+  <div class="modal-backdrop">
+    <div class="modal">
+      <header class="modal-header">
+        <slot name="header">
+          Hi there!
+        </slot>
+      </header>
+
+
+      <section class="modal-body">
+        <slot name="body">
+          My name is JANET
+        </slot>
+       </section>
+
+       <footer class="modal-footer">
+          <slot name="footer">
+            This is my week 3 task
+             <button class="modal-button" @click="$emit('close')">
+                  Close
+                  </button>
+        </slot>
+      </footer>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'modal',
+}
+</script>
+
+
+
+
+<style>
+
+
+.modal-backdrop {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: rgba(0, 0, 0, 0.3);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .modal {
+    background: #FFFFFF;
+    box-shadow: 2px 2px 20px 1px;
+    overflow-x: auto;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .modal-header,
+  .modal-footer {
+    padding: 15px;
+    display: flex;
+  }
+
+  .modal-header {
+    border-bottom: 1px solid #eeeeee;
+    color: #4AAE9B;
+    justify-content: space-between;
+  }
+
+  .modal-footer {
+    border-top: 1px solid #eeeeee;
+    justify-content: flex-end;
+  }
+
+  .modal-body {
+    position: relative;
+    padding: 20px 10px;
+  }
+
+  .btn-close {
+    border: none;
+    font-size: 20px;
+    padding: 20px;
+    cursor: pointer;
+    font-weight: bold;
+    color: #4AAE9B;
+    background: transparent;
+  }
+
+  .btn-green {
+    color: white;
+    background: #4AAE9B;
+    border: 1px solid #4AAE9B;
+    border-radius: 2px;
+  }
+</style>
